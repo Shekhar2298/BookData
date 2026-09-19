37cb00fe94ea2e731244dd1600a1693a6040cf73
Shekhar2298 <131153230+Shekhar2298@users.noreply.github.com>
2026-09-19 10:55:25 +0530

    Polish library UI and add validation

--- src/main/java/com/book/booklibrary/Controller/BookController.java
+++ src/main/java/com/book/booklibrary/Controller/BookController.java
@@ -1,69 +1,39 @@
 package com.book.booklibrary.Controller;
 
-import java.util.List;
-
-// BookController.java
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.book.booklibrary.Model.Book;
+import com.book.booklibrary.Service.BookService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.book.booklibrary.Model.Book;
-import com.book.booklibrary.Service.BookService;
-
 @Controller
 @RequestMapping("/books")
 public class BookController {
+    private final BookService bookService;
 
-    @Autowired
-    private BookService bookService;
+    public BookController(BookService bookService) { this.bookService = bookService; }
 
     @GetMapping
     public String getAllBooks(Model model) {
-        List<Book> books = bookService.getAllBooks();
-        model.addAttribute("books", books);
+        model.addAttribute("books", bookService.getAllBooks());
         return "index";
     }
 
     @GetMapping("/add")
     public String showAddBookForm(Model model) {
         model.addAttribute("book", new Book());
-        return "add-book";
+        return "Add-Book";
     }
 
     @PostMapping("/add")
-    public String addBook(@ModelAttribute Book book) {
+    public String addBook(@Valid @ModelAttribute("book") Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) return "Add-Book";
         bookService.addBook(book);
         return "redirect:/books";
     }
-    // @RestController
-    // @RequestMapping("/api/books")
-    // public class BookController {
-
-    // @Autowired
-    // private BookService bookService;
-
-    // @GetMapping
-    // public ResponseEntity<List<Book>> getAllBooks() {
-    // List<Book> books = bookService.getAllBooks();
-    // return ResponseEntity.ok(books);
-    // }
-
-    // @GetMapping("/add")
-    // public String showAddBookForm(Model model) {
-    // model.addAttribute("book", new Book());
-    // return "add-book";
-    // }
-
-    // @PostMapping(value = "/add", produces = "application/json")
-    // public ResponseEntity<String> addBook(@ModelAttribute Book book) {
-    // bookService.addBook(book);
-    // return ResponseEntity.status(HttpStatus.CREATED).body("Book added
-    // successfully");
-    // }
-    // }
 }
