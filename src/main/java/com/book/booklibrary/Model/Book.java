21fcc9ab9e1030d8c34345643a4268461583d06d
Shekhar2298 <131153230+Shekhar2298@users.noreply.github.com>
2026-09-19 10:55:20 +0530

    Polish library UI and add validation

--- src/main/java/com/book/booklibrary/Model/Book.java
+++ src/main/java/com/book/booklibrary/Model/Book.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,50 +15,20 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "Please enter a book name.")
+    @Size(max = 120, message = "Book name must be 120 characters or fewer.")
     private String name;
+
+    @NotBlank(message = "Please enter a description.")
+    @Size(max = 1000, message = "Description must be 1000 characters or fewer.")
     private String description;
+
+    @Size(max = 500, message = "Image URL must be 500 characters or fewer.")
+    @Pattern(regexp = "^$|https?://.+", message = "Use a valid http:// or https:// image URL.")
     private String imageName;
+
+    @Size(max = 500, message = "Video URL must be 500 characters or fewer.")
+    @Pattern(regexp = "^$|https?://.+", message = "Use a valid http:// or https:// video URL.")
     private String videoUrl;
-
-    // Generated getters and setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImageName() {
-        return imageName;
-    }
-
-    public void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
-
-    public String getVideoUrl() {
-        return videoUrl;
-    }
-
-    public void setVideoUrl(String videoUrl) {
-        this.videoUrl = videoUrl;
-    }
 }
-
